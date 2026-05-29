640b4492a523f1ebf58add3cd0081d422c46e807
Adony <adony@example.com>
2026-05-29 18:19:47 -0400

    fix: restaurar import Color para WebView background

--- anird-android/app/src/tv/java/com/example/anird/tv/TvPlayerActivity.kt
+++ anird-android/app/src/tv/java/com/example/anird/tv/TvPlayerActivity.kt
@@ -1,7 +1,8 @@
 package com.example.anird.tv
 
 import android.annotation.SuppressLint
-import android.content.Intent\r\nimport android.graphics.Color
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
